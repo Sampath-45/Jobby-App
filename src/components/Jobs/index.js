f8e7104412c6a51032009a38bbcf03a0d43c692b
Sampath-45 <laxmisampath806@gmail.com>
2024-12-16 08:59:10 +0000

    first commit

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header'
+import AllJobsSection from '../AllJobsSection'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="jobs-sections">
+      <AllJobsSection />
+    </div>
+  </>
+)
+
+export default Jobs
